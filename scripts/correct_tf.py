346e1e1d4682af6f15c51849867b611dbe2e08fb
David Alejo <daletei@upo.es>
2020-07-21 15:48:58 +0200

    Added a utility to correct // in tfs

---
+++ scripts/correct_tf.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import sys
+import rosbag
+import datetime
+import rospy
+
+if (len(sys.argv) != 3):
+  print 'Usage: {} <input bag> <output bag>'.format(sys.argv[0])
+  sys.exit(-1)
+
+if sys.argv[1] == sys.argv[2]:
+  print 'Error: the input and output files have identical names'
+  sys.exit(-2)
+  
+with rosbag.Bag(sys.argv[2], 'w') as outbag:
+    with rosbag.Bag(sys.argv[1]) as bag:
+        total = bag.get_message_count()
+        print 'Total messages: ',total
+        cont2 = 0
+        changes = 0
+        for topic, msg, t in bag.read_messages():
+            if topic == '/tf' or topic == '/tf_static':
+                for i in msg.transforms:
+                    # print msg.transforms[0].header
+                    if '//' in i.header.frame_id:
+                        i.header.frame_id = i.header.frame_id.replace('//', '/')
+                        changes += 1
+                    if '//' in i.child_frame_id:
+                        i.child_frame_id = i.child_frame_id.replace('//', '/')
+                        changes += 1
+                        
+            if msg._has_header:
+                    
+                if '//' in msg.header.frame_id:
+                    msg.header.frame_id = msg.header.frame_id.replace('//', '/')
+                    changes += 1
+                    
+                    
+                    
+                
+            print 'Completed: ', int((cont2*100)/total),'%  Changes: ', changes
+            print "\033[F\033[F"
+            
+            cont2 += 1
+            outbag.write(topic, msg, t)
+ 
+print 'Corrected tfs. Number of changes: {}'.format(changes)
